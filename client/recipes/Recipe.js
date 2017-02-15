b8f7196d2f4e4f35afb0a55f382a3c58ad642ee4
Josue <josueelias94@gmail.com>
2017-02-14 23:12:38 -0200

    Deleting recipes and session editmode

--- client/recipes/Recipe.js
+++ client/recipes/Recipe.js
@@ -1,5 +1,18 @@
+
+Template.Recipe.helpers({
+    updateRecipeId: function() { 
+      return this._id;
+    }
+});
+
 Template.Recipe.events({
   'click .toggle-menu': function() {
     Meteor.call('toggleMenusItem', this._id, this.inMenu);
-  }
+  },
+  'click .fa-trash': function() {
+    Meteor.call('deleteRecipe', this._id);
+  },
+  'click .fa-pencil': function() {
+    Session.set('editMode', !Session.get('editMode'));
+  },
 });
